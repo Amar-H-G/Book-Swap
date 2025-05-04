308c90e2f5da058ba594ddeb5c7e9665d5be0593
Amar Patra <amarpatra932@gmail.com>
2025-05-04 16:31:36 +0530

    env file creation

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -35,8 +35,8 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server started at http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
 });
 
 module.exports = { app };
