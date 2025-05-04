ddbd6fcc5e5ac66185aa2a3fe43ed49cbc55207b
Amar Patra <amarpatra932@gmail.com>
2025-05-04 17:04:20 +0530

    changes bcrypt to bcryptjs

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,7 +1,7 @@
 // routes/auth.js
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 // Login route
